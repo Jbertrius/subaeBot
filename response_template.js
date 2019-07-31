95d34cbee6ac348aff580222f8e9955e8e3e698e
YR28057 <Eyram Jean-Bertrand EZIN eyram-jean-bertrand.ezin-ext@yrnet.com>
2019-07-31 17:10:15 +0200

    feat: add form simple#

---
+++ response_template.js
@@ -0,0 +1,79 @@
+
+exports.answer_add = `📄Veuillez utiliser la forme suivante📄
+
+⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️`
+
+
+exports.answer_add_2 = `🎫New tool : ECA🎫
+
+[information]
+ 
+NAME : 
+ 
+■ Basic Information 
+- indo(+plus people who ran together): 
+
+- actual(person in charge of communication):
+
+- date of :  
+
+- place of : 
+
+- phone :
+
+■Personal Information 
+- gender: 
+- birthday: 
+- religion:
+- nationality :
+- home location:
+- major: 
+- work:
+- work location: 
+
+■Time & Environment (Specifically) 
+- weekdays: 
+- weekend :
+- vacation : 
+
+■Physical information 
+- personality(Expected enneagram) :
+- interests/Goal/Dream :
+
+■Spiritual information  
+- History of faith (how he uses to live his life of faith) :
+
+- Bible :
+
+- Satisfaction : 
+
+- Will of action :
+
+- Curiousity : 
+
+- Etc : Why the fruit want to come to Bible sharing?
+
+■mannam F/B :   
+- Opportunity: 
+
+- Threat:
+
+- Next meeting:`
+
+exports.answer_add_3 = `🎫New tool : ECA🎫
+
+[information]
+ 
+NAME : 
+ 
+■ Basic Information 
+- indo(+plus people who ran together): 
+
+- actual(person in charge of communication):
+
+- date of :  
+
+- place of : 
+
+- phone :
+`
